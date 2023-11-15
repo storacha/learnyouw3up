c11932686626ec3c61dabdcd39b7302609ce6f73
Alan Shaw <alan.shaw@protocol.ai>
2023-11-15 23:27:39 +0300

    chore: switch to pnpm

--- problems/5-memes/index.js
+++ problems/5-memes/index.js
@@ -1,3 +1,4 @@
+/* eslint n/no-callback-literal: 0 */
 import fs from 'node:fs'
 import path from 'node:path'
 import { Buffer } from 'node:buffer'
--- problems/7-commp/index.js
+++ problems/7-commp/index.js
@@ -30,35 +30,35 @@ export const verify = (args, cb) => {
 
     if (!info.piece) {
       cb(false)
-      return console.error(`Invalid deal info: missing piece`)
+      return console.error('Invalid deal info: missing piece')
     }
     if (!Array.isArray(info.deals)) {
       cb(false)
-      return console.error(`Invalid deal info: non-array deals`)
+      return console.error('Invalid deal info: non-array deals')
     }
 
     console.log(`\n           Piece: ${info.piece}\n`)
     for (const deal of info.deals) {
       if (!deal || typeof deal !== 'object') {
         cb(false)
-        return console.error(`Invalid deal info: non-object deal`)
+        return console.error('Invalid deal info: non-object deal')
       }
 
       if (!deal.provider) {
         cb(false)
-        return console.error(`Invalid deal info: missing provider`)
+        return console.error('Invalid deal info: missing provider')
       }
       console.log(`Storage Provider: f0${deal.provider}`)
 
       if (!deal.aggregate) {
         cb(false)
-        return console.error(`Invalid deal info: missing aggregate CID`)
+        return console.error('Invalid deal info: missing aggregate CID')
       }
       console.log(`       Aggregate: ${deal.aggregate}`)
 
       if (!deal.aux?.dataSource?.dealID) {
         cb(false)
-        return console.error(`Invalid deal info: missing deal ID`)
+        return console.error('Invalid deal info: missing deal ID')
       }
       console.log(`         Deal ID: ${deal.aux.dataSource.dealID}\n`)
     }
