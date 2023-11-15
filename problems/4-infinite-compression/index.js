298ec6f681f9b3d61e91f213c2585aa6ac80249e
Alan Shaw <alan.shaw@protocol.ai>
2023-11-15 15:34:56 +0300

    feat: delegation problem

---
+++ problems/4-infinite-compression/index.js
@@ -0,0 +1,23 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import { execaNode } from 'execa'
+import msee from 'msee'
+
+export const problem = msee.parse(fs.readFileSync(new URL('./problem.md', import.meta.url), 'utf8'))
+
+export const solution = msee.parse(fs.readFileSync(new URL('./solution.md', import.meta.url), 'utf8'))
+
+/**
+ * @param {string[]} args
+ * @param {(success: boolean) => void} cb
+ */
+export const verify = (args, cb) => {
+  (async () => {
+    const filepath = path.resolve(args[0])
+    console.log(`Verifying ${filepath}...\n`)
+    const { stdout, all } = await execaNode(filepath, [], { all: true })
+    console.log(all ?? '')
+
+    cb(false)
+  })()
+}
---
+++ problems/4-infinite-compression/problem.md
@@ -0,0 +1,6 @@
+# Placeholder
+
+─────────────────────────────────────────────────────────────────────────────
+* To print these instructions again, run: `$ADVENTURE_NAME print`
+* To verify your program, run: `$ADVENTURE_NAME verify program.js`
+* For help run: `$ADVENTURE_NAME help`
